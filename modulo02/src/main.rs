6451cf342f216d2b4c07e8e0fd6360083d50d0f1
Bruno C <v3ktoryt@gmail.com>
2024-10-24 23:10:55 -0300

    Modulo 02: 51-63

--- modulo02/src/main.rs
+++ modulo02/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::f32::consts::PI;
 use std::ops::Deref;
 
@@ -356,6 +357,150 @@ fn main() {
 
     // -----------------------------------------
 
+
+    // -- Tarefa Melhor momento para comprar e vender uma acao:
+
+    let valores_semana1 = vec![7, 1, 5, 3, 6, 4, 1];
+    let valores_semana2 = vec![7, 6, 6, 3, 3, 1, 1];
+
+    let maior_semana1 = maior_lucro_acoes( &valores_semana1 );
+    let maior_semana2 = maior_lucro_acoes( &valores_semana2 );
+
+    println!( "\nO maior lucro possivel: {}", maior_semana1 );
+    println!( "O maior lucro possivel: {}", maior_semana2 );
+
+    // -----------------------------------------
+
+
+    // -- Tarefa Movendo os Zeros:
+
+    let mut nums01 = vec![0, 1, 0, 3, 12];
+    let mut nums02 = vec![0, 4, 7, 0, 23];
+
+    move_zeros( &mut nums01 );
+    mover_zeros( &mut nums02 );
+
+    println!( "\nZeros movidos: {:?}", nums01 );
+    println!( "Zeros movidos: {:?}", nums02 );
+
+    // -----------------------------------------
+
+
+    // -- HashMap:
+
+    let mut notas: HashMap<&str, f32> = HashMap::new();
+
+    notas.insert("Matematica", 5.0);
+    notas.insert("Portugues", 7.2);
+    notas.insert("Historia", 6.8);
+    notas.insert("Ciencia", 9.2);
+
+    match notas.get("Matematica") {
+        Some(val) => { println!( "\nNota em de matematica: {}", val ) },
+        None => { println!( "Sem nota em Matematica" ) }
+    }
+
+    notas.remove("Matematica");
+    println!( "Valores: {:?}", notas.values() );
+    println!( "Chaves {:?}", notas.keys() );
+    println!( "Aluno estudou informatica? {}", notas.contains_key("Informatica") );
+
+    for (materia, nota) in notas {
+        println!( "Media de {}: {}", materia, nota );
+    }
+
+    // -----------------------------------------
+
+
+    // -- Tarefa: Encontrar Duplicatas em um Vetor:
+
+    let nums1 = vec![1, 2, 3, 4, 5];
+    let nums2 = vec![1, 2, 3, 4, 1];
+
+    println!( "\nArray 1 contém duplicatas: {}", contains_duplicates(&nums1) );
+    println!( "Array 2 contém duplicatas: {}", contains_duplicates(&nums2) );
+
+    // -----------------------------------------
+
+}
+
+fn contains_duplicates( input: &Vec<i32> ) -> bool {
+    if input.is_empty() { return false }
+
+    let set: HashSet<_> = input.iter().collect();
+
+    !(set.len() == input.len())
+}
+
+fn mover_zeros(nums: &mut Vec<i32>) {
+    let mut indice_nao_zero = 0;
+
+    // Iterar através do array
+    for i in 0..nums.len() {
+        // Se o elemento atual for não zero
+        if nums[i] != 0 {
+            // Trocar o elemento não zero com o elemento no índice indice_nao_zero
+            nums.swap(i, indice_nao_zero);
+            // Avançar o índice indice_nao_zero
+            indice_nao_zero += 1;
+        }
+    }
+}
+
+fn move_zeros( input: &mut Vec<i32> ) {
+    if input.is_empty() { return; }
+
+    for index in 0..input.len() {
+
+        let value = input[index];
+        if value == 0 {
+            input.remove(index);
+            input.push(0);
+        }
+
+    }
+}
+
+fn lucro_maximo(prices: Vec<i32>) -> i32 {
+    if prices.is_empty() {
+        return 0;
+    }
+
+    let mut preco_minimo = prices[0];
+    let mut lucro_maximo = 0;
+
+    for preco in prices.iter().skip(1) {
+        let lucro_atual = preco - preco_minimo;
+        lucro_maximo = lucro_maximo.max(lucro_atual);
+        preco_minimo = preco_minimo.min(*preco);
+    }
+
+    lucro_maximo
+}
+
+fn maior_lucro_acoes( values: &Vec<u32> ) -> u32 {
+
+    let mut lucros: Vec<u32> = vec![];
+    let days = values.len();
+
+    for day in 0..days-1 {
+
+        let compra = values[day];
+        let mut maior_lucro: u32 = 0;
+
+        for vende in values.split_at(day+1).1 {
+            if *vende > compra {
+                let dif = vende - compra;
+                if dif > maior_lucro { maior_lucro = dif; }
+                continue;
+            }
+        }
+
+        lucros.push( maior_lucro );
+    }
+
+    lucros.sort();
+    *lucros.last().unwrap()
 }
 
 fn encontrar_kesimo_maior( nums: &Vec<i32>, k: i32 ) -> i32 {
