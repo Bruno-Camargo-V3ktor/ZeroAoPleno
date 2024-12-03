0471492b0279d15f1adb474b00f35746724ce079
Bruno C <v3ktoryt@gmail.com>
2024-12-03 16:30:22 -0300

    Aula 1-5: Algoritimo de Bubble Sort

--- modulo05/src/main.rs
+++ modulo05/src/main.rs
@@ -1,4 +1,19 @@
 
+
 fn main() {
+
+    // -- Algoritimo de Bubble Sort
+    let mut array: [i32;7] = [10, 23, 4, 5, 66, 7, -3];
+    println!( "{array:?}" );
+
+    for i in 0 .. array.len() {
+        for j in ( i+1 .. array.len() ).rev() {
+            if array[j-1] > array[j] { array.swap(j-1, j); }
+        }
+    }
+
+    println!( "{array:?}" );
+
+    // ---------------------------
 
 }
