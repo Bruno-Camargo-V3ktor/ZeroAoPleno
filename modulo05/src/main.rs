0b9bcbbb72d82e74243d1cd03ce4c21e9286d5a6
Bruno C <v3ktoryt@gmail.com>
2024-12-03 16:46:06 -0300

    Aula 6: (Tarefa) Leitura de arquivo e ordenação de números inteiros com Bubble sort

--- modulo05/src/main.rs
+++ modulo05/src/main.rs
@@ -1,6 +1,19 @@
+use std::{fs::File, io::Read};
 
 
 fn main() {
+
+    // -- Tarefa: Leitura de arquivo e ordenação de números inteiros com Bubble sort
+    println!( "" );
+
+    let path = &String::from( "./nums.txt" );
+    let mut nums: Vec<_> = read_file( path ).split( ',' ).map( |s| s.parse::<i32>().expect( "Numero Invalido" ) ).collect();
+
+    bubble_sort( &mut nums );
+    println!( "{nums:?}" );
+
+    // ---------------------------
+
 
     // -- Algoritimo de Bubble Sort
     let mut array: [i32;7] = [10, 23, 4, 5, 66, 7, -3];
@@ -15,5 +28,24 @@ fn main() {
     println!( "{array:?}" );
 
     // ---------------------------
+
+}
+
+fn read_file(filename: &String) -> String {
+    let mut file = File::open( filename ).expect( "Arquivo nao encontrado." );
+
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).expect( "Erro ao ler arquivo" );
+
+    buf.trim().to_string()
+}
+
+fn bubble_sort( arr: &mut Vec<i32> ) {
+
+    let size = arr.len();
+
+    for i in 0 .. size  {
+        for j in (i+1 .. size).rev() { if arr[j-1] > arr[j] { arr.swap(j-1, j); } }
+    }
 
 }
