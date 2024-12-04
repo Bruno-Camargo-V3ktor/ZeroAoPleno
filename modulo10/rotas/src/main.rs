6e0e8ce98d1623cbf769e62b385c000745f3d19b
Bruno C <v3ktoryt@gmail.com>
2024-12-03 21:38:02 -0300

    Modulo10 - Aula 4: Rotas Dinamicas Com Query Params

--- modulo10/rotas/src/main.rs
+++ modulo10/rotas/src/main.rs
@@ -6,7 +6,7 @@ fn teste() -> &'static str {
 }
 
 // Isso e um 'controller' ele receber requisicao e devolver uma resposta
-// com as <> podemos pegar um valor dinamicamente de nossa rota e passar para a funcao controladora
+// com as <_> podemos pegar um valor dinamicamente de nossa rota e passar para a funcao controladora
 // o nome que esta entre os <...> deve ser o mesmo nome do parametro a qual o valor deve ser atribuido dentro da funcao
 #[ get( "/hello/<name>" ) ]
 fn hello(name: String) -> String {
@@ -18,7 +18,14 @@ fn number(n: i32) -> String {
     format!( "{n} * 2 = {}", n * 2 )
 }
 
+// Rota com Query Params (Parametros de URL) obrigatorios
+#[ get( "/search?<query>&<max_results>&<page>" ) ]
+fn search(query: String, max_results: i32, page: i32) -> String {
+    format!( "Searching for '{query}' (max:{max_results}, page:{page}) " )
+}
+
+
 #[ launch ]
 fn init() -> _ {
-    rocket::build().mount("/", routes![teste, hello, number])
+    rocket::build().mount("/", routes![teste, hello, number, search])
 }
