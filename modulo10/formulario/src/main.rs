249b69c278a238b78a4e17237395c65904601864
Bruno C <v3ktoryt@gmail.com>
2024-12-05 23:00:51 -0300

    Modulo 10 - Aula 12: Corpo do Formulario HTML

--- modulo10/formulario/src/main.rs
+++ modulo10/formulario/src/main.rs
@@ -1,7 +1,6 @@
-use rocket::{ launch, routes };
+use rocket::{ launch, routes, get };
 use rocket::response::content;
-use rocket::http::ContentType;
-use rocket::serde::json::Json;
+//use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
 
 
@@ -11,11 +10,44 @@ struct FormInput {
     password: String
 }
 
+#[ get( "/form" ) ]
+fn form() -> content::RawHtml<String> {
+    let html =
+        r#"
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Formulario</title>
+            </head>
+
+            <body>
+                <h1>Exemplo de Formulario</h1>
+                <form method="post" action="/form">
+
+                    <label for = "name">Nome</ label>
+                    <input type="text" name="name" />
+
+                    <br />
+
+                    <label for = "e-mail">E-mail</ label>
+                    <input type="email" name="e-mail" />
+
+                    <br />
+
+                    <button type = "submit">Enviar</button>
+
+                </form>
+            </body>
+        </html>
+        "#; // r#""# -> Declara uma String literal, alem de pode quebrqa linhas, ele ignora os caracteres especias como \n
+
+    content::RawHtml( html.to_string() )
+}
 
 
 
 #[ launch ]
 fn start() -> _ {
     rocket::build()
-        .mount("/", routes![])
+        .mount("/", routes![form])
 }
