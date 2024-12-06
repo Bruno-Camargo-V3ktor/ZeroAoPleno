852b2ab4e42b8c736e649b229a9d932fc4173630
Bruno C <v3ktoryt@gmail.com>
2024-12-05 23:35:40 -0300

    Modulo 10 - Aula 13-14: Terminado Endpoints para o POST do Formulario

--- modulo10/formulario/src/main.rs
+++ modulo10/formulario/src/main.rs
@@ -1,6 +1,7 @@
-use rocket::{ launch, routes, get };
+use rocket::{ launch, routes, get, post };
 use rocket::response::content;
-//use rocket::serde::json::Json;
+use rocket::http::Status;
+use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
 
 
@@ -8,6 +9,19 @@ use serde::{Deserialize, Serialize};
 struct FormInput {
     username: String,
     password: String
+}
+
+fn validate_input( form_input: &FormInput ) -> Result<(), String> {
+
+    if form_input.username.is_empty() {
+        return Err( "Username is required".to_string() )
+    }
+
+    if form_input.password.is_empty() {
+        return Err( "Password is required".to_string() )
+    }
+
+    Ok(())
 }
 
 #[ get( "/form" ) ]
@@ -22,15 +36,15 @@ fn form() -> content::RawHtml<String> {
 
             <body>
                 <h1>Exemplo de Formulario</h1>
-                <form method="post" action="/form">
+                <form method="post" action="/api/form">
 
                     <label for = "name">Nome</ label>
                     <input type="text" name="name" />
 
                     <br />
 
-                    <label for = "e-mail">E-mail</ label>
-                    <input type="email" name="e-mail" />
+                    <label for = "password">Senha: </ label>
+                    <input type="password" name="password" />
 
                     <br />
 
@@ -44,10 +58,62 @@ fn form() -> content::RawHtml<String> {
     content::RawHtml( html.to_string() )
 }
 
+#[ post( "/form", data="<form_input>" ) ]
+fn submit_form( form_input: Json<FormInput> ) -> Result< content::RawHtml<String>, (Status, content::RawHtml<String>) > {
 
+    match validate_input( &form_input.0 ) {
+        Ok(_) => {
+            let msg = format!( "Username: {} \nPassword: {}", form_input.username, form_input.password );
+
+            let html = content::RawHtml(
+                format!(
+                    r#"
+                    <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>Formulario</title>
+                        </head>
+
+                        <body>
+                            <h1>Sucesso!</h1>
+                            <p>{msg}</p>
+                        </body>
+                    </html>
+                    "#
+                )
+            );
+
+            Ok( html )
+        }
+
+        Err( msg ) => {
+            let html = content::RawHtml(
+                format!(
+                    r#"
+                    <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>Formulario</title>
+                        </head>
+
+                        <body>
+                            <h1>Error!</h1>
+                            <p>{msg}</p>
+                            <p> <a href="/api/form">Voltar</a> </p>
+                        </body>
+                    </html>
+                    "#
+                )
+            );
+
+            Err( (Status::UnprocessableEntity, html) )
+        }
+    }
+
+}
 
 #[ launch ]
 fn start() -> _ {
     rocket::build()
-        .mount("/", routes![form])
+        .mount("/api/", routes![form, submit_form])
 }
