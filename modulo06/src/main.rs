9def33ad304d483941b927cb91fc6969fa78fb2e
Bruno C <v3ktoryt@gmail.com>
2024-12-15 21:15:55 -0300

    Modulo 06 - Aula 03-04: Exemplos de uso das Threads

--- modulo06/src/main.rs
+++ modulo06/src/main.rs
@@ -2,6 +2,38 @@ use std::thread;
 
 fn main() {
 
+    // --/> Exemplos de Uso das Threads
+
+    let handle1 = thread::spawn( || {
+        hello_thread(1);
+    } );
+
+    let handle2 = thread::spawn( || {
+        hello_thread(2);
+    } );
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+
+    println!( "Fim das Threads..." );
+
+    // A funcao join do JoinHandle, ira retorna um result, que caso de tudo certo na
+    // "esperar" na thread atual, ele ira retorna um Ok com o Result que eh o retorno da Thread execuatda
+    // Caso aconteca algum erro, ele ira retorna um Err
+
+    let result = thread::spawn( || {
+        Err::<i32, &str>("Algo deu errado")
+    } ).join().unwrap();
+
+    match result {
+        Ok( value ) => println!( "Thread bem sucedida com valor: {value:?}" ),
+        Err( err ) => println!( "Erro na thread: {err:?}" )
+    }
+
+    println!();
+    // ----
+
+
     // --/> Introdução as Threads
 
     thread::spawn( || {
@@ -12,4 +44,8 @@ fn main() {
 
     // ----
 
+}
+
+fn hello_thread(id: i32) {
+    println!("Bem vindo a Thread: {id}");
 }
